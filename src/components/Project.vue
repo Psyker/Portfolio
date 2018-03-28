ae07c348889df59b8526eb2cb463eef564a77e0c
Psyker <bourgoin.theo1@gmail.com>
2018-03-28 21:00:45 +0200

    Initial commit, setup project

---
+++ src/components/Project.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'project',
+  props: {
+    thumbnail: {
+        required: true,
+        type: String
+    },
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
